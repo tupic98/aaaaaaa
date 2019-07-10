ad4100671abf8d11b75c9af9921259722e2c6b72
petrlr14 <pedrodeveloper14@gmail.com>
2019-07-10 14:01:20 -0600

    init

--- src/com/uca/capas/configuration/JpaConfiguration.java
+++ src/com/uca/capas/configuration/JpaConfiguration.java
@@ -1,5 +1,63 @@
 package com.uca.capas.configuration;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.uca.capas.repositories")
 public class JpaConfiguration {
 
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPersistenceUnitName("capas");
+		em.setPackagesToScan("com.uca.capas.domain");
+
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(hibernateProperties());
+
+		return em;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/parcial");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("root");
+		return dataSource;
+	}
+
+	Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
+		return properties;
+
+	}
+
+	@Bean
+	JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
+	}
+	
 }
--- src/com/uca/capas/configuration/SpringInitialializer.java
+++ src/com/uca/capas/configuration/SpringInitialializer.java
@@ -1,5 +1,23 @@
 package com.uca.capas.configuration;
 
-public class SpringInitialializer {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class SpringInitialializer implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(SpringConfiguration.class);
+		ctx.setServletContext(servletContext);
+		ServletRegistration.Dynamic serDynamic = servletContext.addServlet("spring-capas", new DispatcherServlet(ctx));
+		serDynamic.addMapping("/");
+		serDynamic.setLoadOnStartup(1);
+	}
+	
 }
